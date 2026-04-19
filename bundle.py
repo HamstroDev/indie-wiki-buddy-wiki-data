a2a0028e227b884bda76e20c3d45f0c0f4f5acb7
violine1101 <violine1101@gmail.com>
2026-04-19 19:16:47 +0200

    Add breezewiki-instances as submodule

--- bundle.py
+++ bundle.py
@@ -1,8 +1,8 @@
 #! /bin/python3
 
-# This script bundles all wiki definitions into a single `data.json` file.
+# This script bundles all data needed by IWB into a single `data.json` file.
 # It also generates a `meta.json` file describing `data.json`, which the
-# extension queries to check whether it should update its cached definitions.
+# extension queries to check whether it should update its cached data.
 
 import hashlib
 import json
@@ -12,10 +12,12 @@ import tomllib
 
 from pathlib import Path
 
-format_version = 0
+format_version = "4.0-dev"
+
+with open('breezewiki-instances/instances.json', 'r') as instances_json:
+    breezewiki_instances = json.load(instances_json)
 
 wikis = {}
-
 for e in os.scandir('wikis'):
     if e.is_file() and e.name.endswith('.toml'):
         id = re.sub(r'\.toml$', '', e.name)
@@ -24,7 +26,8 @@ for e in os.scandir('wikis'):
 
 data = {
     'format_version': format_version,
-    'hosts': [],
+    'breezewiki_instances': breezewiki_instances,
+    'hosts': {},
     'wikis': wikis,
 }
 
@@ -39,12 +42,9 @@ with open('generated/data.json', 'w+') as data_json:
 with open('generated/data.json', 'rb') as data_json:
     hash = hashlib.file_digest(data_json, 'sha256').hexdigest()
 
-    with open('generated/meta.json', 'w+') as meta_json:
-        meta = {
-            'format_version': format_version,
-            'hash': hash,
-        }
-        compact_json_dump(meta, meta_json)
-
-
-
+with open('generated/meta.json', 'w+') as meta_json:
+    meta = {
+        'format_version': format_version,
+        'hash': hash,
+    }
+    compact_json_dump(meta, meta_json)
